0788db30d31ee48a1a3b70a528c45dc0c908976b
dankarger <karger.dan@gmail.com>
2022-11-20 20:35:26 +0200

    still problems with eq paramaters

--- src/components/Eq/Eq.jsx
+++ src/components/Eq/Eq.jsx
@@ -3,8 +3,9 @@ import {useEffect, useState} from "react";
 
 const Eq=({context, number, gain ,source})=>{
     const [highPassState, setHighPassState] = useState()
-    const [highPassHz, setHighPassHz] = useState( 1500)
+    const [highPassHz, setHighPassHz] = useState( 10)
     const [highPassGain, setHighPassGain] = useState(0)
+    const [ highPassQ, setHighPassQ] = useState(100)
     const [biQuadFreq, setBiQuadFreq] = useState(400)
     const [biQuadGain, setBiQuadGain] = useState(25)
 
@@ -35,9 +36,10 @@ const Eq=({context, number, gain ,source})=>{
         highPass.connect(gain)
         // highPassGain.connect(gain)
 
-        highPass.type = "notch"
-        highPass.frequency.value = 1550
+        highPass.type = "peaking"
+        highPass.frequency.value = 1000
         highPass.gain.value = 0
+        highPass.Q.value = 1
         setHighPassState(highPass)
 
         //
@@ -54,17 +56,32 @@ const Eq=({context, number, gain ,source})=>{
         //
         // lowPass.type = "lowpass"
         // lowPass.connect(gain);
-    },[context,gain, source])
+    },[context,gain, source ])
+
+    // useEffect(()=>{
+    //     highPassState.connect(gain)
+    //
+    // },[highPassState, gain])
+
 
     const changeParams = (e)=> {
         setHighPassHz(e.target.value)
         highPassState.frequency.value = e.target.value
+        // highPassState.frequency.setValueAtTime(e.target.value,context.currentTime)
     }
     const changeGain = (e) => {
         setHighPassGain(e.target.value)
-        highPassState.gain.value = e.target.value
-    }
+        console.log('1',highPassState)
+        // highPassState.gain.value = e.target.value
+        highPassState.gain.setValueAtTime(e.target.value,context.currentTime)
+        console.log('2',highPassState)
 
+    }
+ const changeQ=(e)=>{
+        setHighPassQ(e.target.value)
+        // highPassState.q = e.target.value
+     highPassState.Q.setValueAtTime(e.target.value,context.currentTime)
+ }
     return (
         <div className={styles.eq}>
 
@@ -83,11 +100,12 @@ const Eq=({context, number, gain ,source})=>{
                     <div className="range-slider">
                         gain
                         <span className="scope">5.9</span>
-                        <input type="range" className="vertical" min="-10" max="25" step="1" value={highPassGain}
+                        <input type="range" className="vertical" min={-1440} max={40} step={1} value={highPassGain}
                                data-filter="highPass-gain" data-param="gain" onChange={changeGain} />
                         <span className="scope scope-min">0.8</span>
                         <span className="param">DB</span>
                         {highPassGain}
+                        <input type="range" min={0.0001} max={1000} onChange={changeQ} value={highPassQ}/>Q
                     </div>
                     {/*<div className="range-slider">*/}
                     {/*    <span className="scope">12</span>*/}
